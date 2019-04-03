95df9f3771f9f487c000dbb2d7134b975cfc2eb5
aikiryu <aikiryu1228@naver.com>
2019-04-03 21:28:07 +0900

    2019.04.03 Test

--- Step_04_Test/Step_04_Test/Main.cpp
+++ Step_04_Test/Step_04_Test/Main.cpp
@@ -16,7 +16,7 @@ int main(void)
 		* Tip.
 
 			* 키(Cm) /= 100; = [미터로 변환]
-			* 비만지수(BMI) = 몸무게(Kg) / ( 키(Cm) * 키(Cm) )
+			* 비만지수(fBmi) = 몸무게(Kg) / ( 키(Cm) * 키(Cm) )
 
 			* float 형과 double 형을 확인하여 작성.
 
@@ -28,51 +28,42 @@ int main(void)
 
 	*******************************************/
 
-	float height = 0.0;
-	float weight = 0.0;
-	float bmi = 0.0;
+	//** 헝가리안 표기법 
+	float fHeight = 0.0;
+	float fWeight = 0.0;
+	float fBmi = 0.0;
 
 
 
-	printf_s("몸무게를 입력하세요 \n");
-	scanf_s("%f", &weight);
+	printf_s("몸무게를 입력하세요 : ");
+	scanf_s("%f", &fWeight);
 
-	printf_s("키를 입력하세요 \n");
-	scanf_s("%f", &height);
-
-
-	height /= 100;
+	printf_s("키를 입력하세요 : ");
+	scanf_s("%f", &fHeight);
 
 
-	bmi = weight / (height * height);
+	fHeight /= 100;
+	fBmi = fWeight / (fHeight * fHeight);
 
-	/*printf_s("%f \n", bmi);
-	printf_s("%f \n", weight);
-	printf_s("%f \n", height);*/
-
-	if (bmi >= 30.00)
+	if (fBmi < 18.5)
 	{
-		printf_s("고도 비만 입니다. \n");
-		
+		printf_s("저체중 입니다. \n");
 	}
-	else if (bmi >= 25.00)
-	{
-		printf_s("비만 입니다. \n");
-	}
-
-	else if (bmi >= 23.00)
-	{
-		printf_s("과체중 입니다. \n");
-	}
-
-	else if (bmi >= 18.5)
+	else if (fBmi >= 18.5 && fBmi <= 22.99)
 	{
 		printf_s("정상 입니다. \n");
 	}
-
+	else if (fBmi >= 23.00 && fBmi <= 24.99)
+	{
+		printf_s("과체중 입니다. \n");
+	}
+	else if (fBmi >= 25.00 && fBmi <= 29.99)
+	{
+		printf_s("비만 입니다. \n");
+	}
 	else 
 	{
-		printf_s("저체중 입니다. \n");
+		printf_s("고도 비만 입니다. \n");
 	}
 
 	system("pause");
