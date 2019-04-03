a0f4bed001bc43e27f5d58ba647d95a5a0c9d0f3
Lenauki <lenauki@naver.com>
2019-04-03 21:20:33 +0900

    이준bmiTest

--- Step_04_Test/Step_04_Test/Main.cpp
+++ Step_04_Test/Step_04_Test/Main.cpp
@@ -28,12 +28,53 @@ int main(void)
 
 	*******************************************/
 
+	float height = 0.0;
+	float weight = 0.0;
+	float bmi = 0.0;
 
 
 
+	printf_s("몸무게를 입력하세요 \n");
+	scanf_s("%f", &weight);
+
+	printf_s("키를 입력하세요 \n");
+	scanf_s("%f", &height);
 
 
+	height /= 100;
 
 
+	bmi = weight / (height * height);
+
+	/*printf_s("%f \n", bmi);
+	printf_s("%f \n", weight);
+	printf_s("%f \n", height);*/
+
+	if (bmi >= 30.00)
+	{
+		printf_s("고도 비만 입니다. \n");
+		
+	}
+	else if (bmi >= 25.00)
+	{
+		printf_s("비만 입니다. \n");
+	}
+
+	else if (bmi >= 23.00)
+	{
+		printf_s("과체중 입니다. \n");
+	}
+
+	else if (bmi >= 18.5)
+	{
+		printf_s("정상 입니다. \n");
+	}
+
+	else 
+	{
+		printf_s("저체중 입니다. \n");
+	}
+
+	system("pause");
 	return 0;
 }
