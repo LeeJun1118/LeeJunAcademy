d01fe42b7d79a66c269388a90aed704900c00bff
LeeJun <ejun1118@gmail.com>
2019-04-29 19:05:42 +0900

    20190429

---
+++ Class/Step_07/Step_07/Main.cpp
@@ -0,0 +1,177 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/********************************************************
+
+	** [010.3958.7179]
+
+
+	** [필수 사항]
+
+	* 오픈톡 참여(강제) : https://open.kakao.com/o/g6HuIslb
+
+	* 참여시 프로필 설정은 비공개로 해도 됩니다. 
+	* 본명 말고 닉네임 적으셔도 됩니다.
+
+	* 단톡방에 하기 어려운 말은 꼭 개인톡으로 해주시고,
+		개인톡으로 말씀하실때 꼭 누군지 먼저 이야기 해주세요~
+
+********************************************************/
+
+
+int main(void)
+{
+	/*********************
+		* 배열과 포인터
+	**********************/
+
+	//**  [실습]
+	
+	/*
+	int iNum1 = 10;
+	int iNum2 = 20;
+
+	iNum1 = iNum2;
+
+	int *piNum1 = &iNum1;
+
+	int *piNum2;
+
+	piNum2 = piNum1;
+	*/
+	
+
+
+
+
+	/*
+	int iArray[5] = { 0, 1, 2, 3, 4 };
+
+	int *piPointer = iArray;
+
+	printf_s("%d\n", *(piPointer++));
+	printf_s("%d\n", *(piPointer++));
+	printf_s("%d\n", *(piPointer));
+	printf_s("%d\n", *(piPointer + 1));
+	printf_s("%d\n", *(piPointer + 2));
+	printf_s("%d\n", *(piPointer + 3));	// <= 쓰레기 값
+	
+	system("pause");
+	*/
+	
+	
+
+
+
+
+
+
+
+	
+
+	/*
+
+	int iArray[5] = { 0, 1, 2, 3, 4 };
+	int* piter = iArray;
+
+	for (int i = 0; i < 5; i++)
+	{
+		printf_s("%d\n", *(piter + i));
+	}
+	system("pause");
+	*/
+	
+
+
+	/*
+	int iArray[5] = { 0, 1, 2, 3, 4 };
+	int *piPointer = iArray;
+
+	for (int i = 0; i < 5; i++)
+	{
+		printf_s("%d\n", piPointer[i]);
+	}
+	system("pause");
+	*/
+
+
+
+	/********************************************************
+		** [배열과 문자열]
+
+		* [PPT 참고 ->  (../Resource/배열_문자열.png)]
+
+
+
+		* [서식 문자] 
+		
+			%c = 문자 출력
+			%s = 문자열 출력
+
+	********************************************************/
+
+	//**  [실습]
+	
+
+	/*
+	char cArray[6] = { 'H', 'e', 'l', 'l', 'o', '\0' };
+	char* piter = cArray;
+
+	for (int i = 0; i < 6; i++)
+	{
+		printf_s("%c", *(piter + i));
+	}
+	printf_s("\n");
+
+	printf_s("%s\n", cArray);
+
+	system("pause");
+	*/
+	
+
+
+
+	/*
+	char cString[] = { 'H', 'i', 0, 'T', 'h', 'e', 'r', 'e', 32 };
+	
+	for (int i = 0; i < 9; i++)
+	{
+		printf_s("%c", cString[i]);
+	}
+	printf_s("\n");
+
+	printf_s("%s\n", cString);
+
+	system("pause");
+	*/
+	
+
+
+
+
+
+	/********************************************************
+		** [포인터 문자열]
+
+		* [PPT 참고 ->  (.. / Resource / 배열_문자열.png)]
+
+	********************************************************/
+
+	//**  [실습]
+
+	/*
+	char* pcString = (char*)"홍길동";
+	
+	printf_s("%s", pcString);
+	printf_s("%d\n", sizeof(pcString));
+
+	printf_s("%d\n", sizeof(__int64));
+
+
+
+	system("pause");
+	*/
+	
+
+	return 0;
+}
---
+++ Class/Step_08/Step_08/Main.cpp
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+struct tagStudent
+{
+	char* pcName;
+	int iKor;
+	int iEng;
+	int iMath;
+};
+
+
+int main(void)
+{
+	/*
+	tagStudent tStudent;
+
+	tStudent.iKor =	1;
+	tStudent.iEng =	10;
+	tStudent.iMath = 100;
+	
+	printf_s("%d\n", tStudent.iKor);
+	printf_s("%d\n", tStudent.iEng);
+	printf_s("%d\n", tStudent.iMath);
+
+	system("pause");
+	*/
+
+	
+
+	
+
+
+	/*************************************
+		** [구조체 배열 사용]
+
+		* 학생 성적 관리 프로그램 만들기.
+
+	**************************************/
+	/*
+	tagStudent tStudent[3];
+
+	for (int i = 0; i < 3; i++)
+	{
+		printf_s("국어 점수 입력 : ");
+		scanf_s("%d", &tStudent[i].iKor);
+
+		printf_s("영어 점수 입력 : ");
+		scanf_s("%d", &tStudent[i].iEng);
+
+		printf_s("수학 점수 입력 : ");
+		scanf_s("%d", &tStudent[i].iMath);
+	}
+
+	for (int i = 0; i < 3; i++)
+	{
+		printf_s("국어 점수 : %d\n", tStudent[i].iKor);
+		printf_s("영어 점수 : %d\n", tStudent[i].iEng);
+		printf_s("수학 점수 : %d\n", tStudent[i].iMath);
+		printf_s("\n");
+	}
+	*/
+	
+
+	system("pause");
+
+	return 0;
+}
+
+
+
+
+
+
