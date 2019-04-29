3933f56077e1e720c0a47e7d407ea3de04fda7d8
LeeJun <ejun1118@gmail.com>
2019-04-29 21:57:03 +0900

    20190429-1

---
+++ step_11/step_11/Headers.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#define _CRT_SECURE_NO_WARNINGS
+
+//프로젝트 외부에 있음
+#include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
+#include <time.h>
+#include <string>
+
+//프로젝트 내부에 있음
+#include "Struct.h"  
